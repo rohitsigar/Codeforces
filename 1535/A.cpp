e6b49e3beb37c98c81568d3d0a3f53e64cf73a87
rohit sigar <rohitsigar05@gmail.com>
2022-06-20 16:49:45 +0530

    Add solution for 1535A

---
+++ 1535/A.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define rall(x)           x.rbegin(), x.rend()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define repi(i,a,b)       for(int i=b-1;i>=a;i--)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define sz5               100005
+#define sz6               1000005
+#define V(a)              vector<a>
+#define MP               make_pair
+#define MT               make_tuple
+#define G(a,b)           get<a>(b)
+const long long INF = 1e18;
+const int32_t M = 1e9 + 7;
+const int32_t MM = 998244353;
+const int N = 5e5 + 5;
+template<typename T1, typename T2>istream& operator>>(istream& in, pair<T1, T2> &a) {in >> a.fr >> a.sc; return in;}
+template<typename T1, typename T2>ostream& operator<<(ostream& out, pair<T1, T2> a) {out << a.fr << " " << a.sc; return out;}
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+void solve(void)
+{
+      string s;
+
+      vector<pair<int, int>> arr;
+      rep(i, 0, 4)
+      {
+            int temp;
+            cin >> temp;
+            arr.pb({temp, i + 1});
+      }
+      sort(arr.begin() , arr.end());
+
+      // rep(i, 0, 4)
+      // {
+      //       cout << arr[i].first << " " << arr[i].second << endl;
+      // }
+
+      if (arr[3].second >= 1 and arr[3].second < 3)
+      {
+            if (arr[2].second > 2 and arr[2].second  < 5)
+            {
+                  cout << "YES" << endl;
+            }
+            else
+            {
+                  cout << "NO" << endl;
+            }
+
+      }
+      else
+      {
+            if (arr[2].second > 2 and arr[2].second  < 5)
+            {
+                  cout << "NO" << endl;
+            }
+            else
+            {
+                  cout << "YES" << endl;
+            }
+
+
+      }
+
+
+}
+signed main()
+{
+#ifndef ONLINE_JUDGE
+      //FOR GETTING INPUT FROM INPUT.TEXT
+      freopen("input", "r", stdin);
+      //for getting output to output.txt
+      freopen("output1", "w", stdout);
+#endif
+      FAST
+      int t;
+      cin >> t;
+      while (t--)
+      {
+            solve();
+      }
+
+      return 0;
+}
