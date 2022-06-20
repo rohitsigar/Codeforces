39207f1308aec5566987705e2f6566f01c0a90b6
rohit sigar <rohitsigar05@gmail.com>
2022-06-20 16:50:47 +0530

    Add solution for 1370C

---
+++ 1370/C.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define rall(x)           x.rbegin(), x.rend()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define repi(i,a,b)       for(ll i=b-1;i>=a;i--)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define sz5               100005
+#define sz6               1000005
+#define V(a)              vector<a>
+#define MP               make_pair
+#define MT               make_tuple
+#define G(a,b)           get<a>(b)
+const long long INF = 1e18;
+const int32_t M = 1e9 + 7;
+const int32_t MM = 998244353;
+const int N = 1e5 + 5;
+template<typename T1, typename T2>istream& operator>>(istream& in, pair<T1, T2> &a) {in >> a.fr >> a.sc; return in;}
+template<typename T1, typename T2>ostream& operator<<(ostream& out, pair<T1, T2> a) {out << a.fr << " " << a.sc; return out;}
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+int val[N];
+
+bool findPrimeFactors(int n)
+{
+	int count = 0;
+	int temp1 = 1;
+	while (n % 2 == 0)
+	{
+		temp1 = temp1 * 2;
+		n = n / 2;
+	}
+
+	// n must be odd at this point. So we can skip
+	// one element (Note i = i +2)
+	for (int i = 3; i <= sqrt(n); i = i + 2)
+	{
+		// While i divides n, print i and divide n
+		while (n % i == 0)
+		{
+			count++;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		count++;
+	}
+
+
+
+	if (temp1 == 2 and count < 2)
+	{
+		return false;
+	}
+	if (count > 0)
+	{
+		return true;
+	}
+	return false;
+
+}
+void solve()
+{
+
+	int n;
+	cin >> n;
+	if (n == 1)
+	{
+		cout << "FastestFinger" << endl;
+		return;
+	}
+
+
+	if (n == 2 || n % 2 != 0 || findPrimeFactors(n))
+	{
+		cout << "Ashishgup" << endl;
+	}
+	else
+	{
+		cout << "FastestFinger" << endl;
+
+	}
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+	//FOR GETTING INPUT FROM INPUT.TEXT
+	freopen("input", "r", stdin);
+	//for getting output to output.txt
+	freopen("output1", "w", stdout);
+#endif
+	FAST
+	int t;
+	cin >> t;
+	// t = 1;
+
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
